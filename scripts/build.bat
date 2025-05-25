0a2ddab6db2907f13ea28dabe5f15f3299db0740
constLiakos <greendayhlias@gmail.com>
2025-05-26 00:39:00 +0300

    update bat script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-pyinstaller --onefile --windowed --name "AI-Launcher" ^
+python -m PyInstaller --onefile --windowed --name "AI-Launcher" ^
     --hidden-import "pynput.keyboard" ^
     --hidden-import "pynput.keyboard._win32" ^
     --hidden-import "pynput._util.win32" ^
