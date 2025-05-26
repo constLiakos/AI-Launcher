e4592613480af77392099988222a349bfd36a021
constLiakos <greendayhlias@gmail.com>
2025-05-26 12:09:12 +0300

    add icon on windows .exe

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,7 @@
 @echo off
 
 python -m PyInstaller --onefile --windowed --name "AI-Launcher" ^
+    --icon "assets/icon.ico" ^
     --hidden-import "pynput.keyboard" ^
     --hidden-import "pynput.keyboard._win32" ^
     --hidden-import "pynput._util.win32" ^
