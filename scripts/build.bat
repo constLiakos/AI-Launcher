a9e450933737b45a363371988543b179e3ba1a26
constLiakos <greendayhlias@gmail.com>
2025-06-06 00:59:22 +0300

    update build bat file

--- scripts/build.bat
+++ scripts/build.bat
@@ -9,5 +9,28 @@ python -m PyInstaller ^
     --hidden-import "pynput.keyboard" ^
     --hidden-import "pynput.keyboard._win32" ^
     --hidden-import "pynput._util.win32" ^
+    --exclude-module "matplotlib" ^
+    --exclude-module "tkinter" ^
+    --exclude-module "test" ^
+    --exclude-module "unittest" ^
+    --exclude-module "doctest" ^
+    --exclude-module "pydoc" ^
+    --exclude-module "pydantic.color" ^
+    --exclude-module "pydantic.json" ^
+    --exclude-module "pydantic.schema" ^
+    --exclude-module "pydantic.mypy" ^
+    --exclude-module "pydantic.networks" ^
+    --exclude-module "email_validator" ^
+    --exclude-module "sqlalchemy.dialects.mysql" ^
+    --exclude-module "sqlalchemy.dialects.postgresql" ^
+    --exclude-module "sqlalchemy.dialects.oracle" ^
+    --exclude-module "sqlalchemy.dialects.mssql" ^
+    --exclude-module "sqlalchemy.dialects.firebird" ^
+    --exclude-module "sqlalchemy.dialects.sybase" ^
+    --exclude-module "PIL" ^
+    --exclude-module "pandas" ^
+    --exclude-module "scipy" ^
+    --exclude-module "IPython" ^
     --collect-all pynput ^
+    --optimize 2 ^
     src/main.py
