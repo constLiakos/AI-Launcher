a8804501afc4bc7d36ba2f5c02a5af17653c6176
constLiakos <greendayhlias@gmail.com>
2025-06-06 00:50:47 +0300

    update pyinstaller excludes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,6 @@
 
 pyinstaller \
     --onefile \
-    --windowed \
     --add-data "src/assets:assets" \
     --name "AI-Launcher" \
     --hidden-import "pynput.keyboard" \
@@ -10,5 +9,28 @@ pyinstaller \
     --hidden-import "pynput._util.xorg" \
     --hidden-import "Xlib.display" \
     --hidden-import "Xlib.X" \
+    --exclude-module matplotlib \
+    --exclude-module tkinter \
+    --exclude-module test \
+    --exclude-module unittest \
+    --exclude-module doctest \
+    --exclude-module pydoc \
+    --exclude-module pydantic.color \
+    --exclude-module pydantic.json \
+    --exclude-module pydantic.schema \
+    --exclude-module pydantic.mypy \
+    --exclude-module pydantic.networks \
+    --exclude-module email_validator \
+    --exclude-module sqlalchemy.dialects.mysql \
+    --exclude-module sqlalchemy.dialects.postgresql \
+    --exclude-module sqlalchemy.dialects.oracle \
+    --exclude-module sqlalchemy.dialects.mssql \
+    --exclude-module sqlalchemy.dialects.firebird \
+    --exclude-module sqlalchemy.dialects.sybase \
+    --exclude-module PIL \
+    --exclude-module pandas \
+    --exclude-module scipy \
+    --exclude-module IPython \
     --collect-all pynput \
+    --optimize 2 \
     src/main.py
